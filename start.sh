d4c74d453566e27d7c52b79451e275bc619a9d5b
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-07-02 16:05:31 +0530

    feat: remove .env.example and update start.sh to ensure environment variables are set correctly

--- start.sh
+++ start.sh
@@ -8,4 +8,17 @@ else
   pip install -r requirements.txt
 fi
 
+cd ..
+cd ..
+
+if [ -f "set_env_vars.sh" ]; then
+  echo "Setting environment variables..."
+  source set_env_vars.sh
+else
+  echo "set_env_vars.sh not found. Terminating script."
+  exit 1
+fi
+
+cd pub/fork-this-idea-slack-bot
+
 python app.py
